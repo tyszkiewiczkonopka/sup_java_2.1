df1cb3074536778844f278ced74add78eef7e0c2
Magdalena <tyszkiewicz.konopqa@gmail.com>
2023-10-10 18:20:21 +0200

    Starting all over again

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -1,2 +1,47 @@
-package org.example;public class Employee {
+package org.example;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private int salary;
+
+    @Override
+    public String toString() {
+        return "Salary for " + firstName + " " + lastName + " is " + salary;
+    }
+
+    public Employee(String firstName, String lastName, int salary) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+
+
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("First name");
+        String firstName = scanner.nextLine();
+        System.out.println("Last name");
+        String lastName = scanner.nextLine();
+        System.out.println("Salary");
+        int salary = scanner.nextInt();
+
+        Employee employee = new Employee(firstName, lastName, salary);
+
+
     }
 }
